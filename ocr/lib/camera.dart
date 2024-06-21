5afbc9642d832e699704dc3e2bd4a4d654deaa58
Tachi0824 <Shinnosuke0824@gmail.com>
2024-06-21 11:50:11 +0900

    camera wakaran

--- ocr/lib/camera.dart
+++ ocr/lib/camera.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-class CameraPage extends StatelessWidget {
-  //const CameraPage({Key? key}) : super(key: key);
+class CameraPage extends StatefulWidget {
+  const CameraPage({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  CameraPageState createState() => CameraPageState();
+}
+
+class CameraPageState extends State<CameraPage> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      // カメラを指定
+      widget.camera,
+      // 解像度を定義
+      ResolutionPreset.medium,
+    );
+
+    // コントローラーを初期化
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // ウィジェットが破棄されたら、コントローラーを破棄
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          "Camera Page",
-          style: TextStyle(
-            fontSize: 20
-          ),
-        ),
-      ),
+    // FutureBuilder で初期化を待ってからプレビューを表示（それまではインジケータを表示）
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return CameraPreview(_controller);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
-}
