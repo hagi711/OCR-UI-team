9c54c83bdbef95fd7d098299414f3fca814241b9
hagi711 <hagiiku2005@icloud.com>
2024-06-21 11:46:08 +0900

    import

--- ocr/lib/import.dart
+++ ocr/lib/import.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ImportPage extends StatelessWidget {
+class ImportPage extends StatefulWidget {
+  const ImportPage({Key? key}) : super(key: key);
+  @override
+  _ImportPageState createState() => _ImportPageState();
+}
+
+class _ImportPageState extends State {
+  File? image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final XFile? _image = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (_image != null) {
+        image = File(_image.path);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('読み込む画像を選んでください'),
+      ),
       body: Center(
-        child: Text(
-          "Import Page",
-          style: TextStyle(
-            fontSize: 20
-          ),
-        ),
+        child: image == null
+            ? const Text('画像がありません')
+            : Image.file(image!, fit: BoxFit.cover),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          getImage();
+        },
+        child: const Icon(Icons.photo),
       ),
     );
   }
